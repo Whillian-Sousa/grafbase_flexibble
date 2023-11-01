07a0e15663812c143142401c327b54957a5604d0
Whillian-Sousa <whillscf@gmail.com>
2023-11-01 03:58:06 -0300

    other commit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,22 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import { NavLinks } from '@/constants'
-import AuthProviders from './AuthProviders'
+import Link from "next/link";
+import Image from "next/image";
+import { NavLinks } from "@/constants";
+import AuthProviders from "./AuthProviders";
+import { getCurrentUser } from "@/lib/session";
+import { signOut } from 'next-auth/react'
+import ProfileMenu from "./ProfileMenu";
 
-const Navbar = () => {
-  const session = {}
+const Navbar = async () => {
+  const session = await getCurrentUser();
+
   return (
     <nav className="flexBetween navbar">
-      <div className='flex-1 felxStart gap-10'>
-        <Link href='/'>
-          <Image src='/logo.svg' width={115} height={43} alt='Flexibble'/>
+      <div className="flex-1 felxStart gap-10">
+        <Link href="/">
+          <Image src="/logo.svg" width={115} height={43} alt="Flexibble" />
         </Link>
         <ul className="xl:flex hidden text-small gap-7">
-          {NavLinks.map((link)=>(
+          {NavLinks.map((link) => (
             <Link href={link.href} key={link.key}>
               {link.text}
             </Link>
@@ -20,16 +24,18 @@ const Navbar = () => {
         </ul>
       </div>
       <div className="flexCenter gap-4">
-        {session ? (
+        {session?.user ? (
           <>
-            <Link href='/create-project'></Link>
+            <ProfileMenu session={session}/>
+
+            <Link href="/create-project">Share Work</Link>
           </>
         ) : (
-          <AuthProviders/>
+          <AuthProviders />
         )}
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
